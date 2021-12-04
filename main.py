fc3a358b5bc6fef2f43eed8880fb4ed4292f3571
peter githinji012 <githinjipeter012 @gmail.com>
2021-12-04 18:42:18 +0300

    python project

---
+++ main.py
@@ -0,0 +1,11 @@
+average_grade=int(input ("average grade"))
+if average_grade>=80:
+    print("A")
+elif average_grade>=60:
+    print("B")
+elif average_grade>=40:
+    print("C")
+elif average_grade>=20:
+    print("D")
+else:
+    print("E")
